6f5c3d63c3dfca66c55d784f41bc547c204ed65e
AidaSofic <ssalihbegovic.aida@gmail.com>
2016-04-06 16:44:19 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scan = new Scanner (System.in);
+
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+
+            int k = m-1;
+            int l = n-1;
+                System.out.println(k);
+                System.out.print(l);
+
+            }
+        }
